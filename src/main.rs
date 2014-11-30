7d600c36bf52cd021be26efe7935eea5fa06e26c
Colin Perkins <csp@csperkins.org>
2014-11-30 09:56:21 +0000

    Add version function, taken from https://github.com/BurntSushi/xsv/blob/master/src/util.rs
    According to the licence at
    https://github.com/BurntSushi/xsv/blob/master/UNLICENSE this "is free and
    unencumbered software released into the public domain".

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 // ================================================================================================
 
+pub fn version() -> String {
+    let (maj, min, pat) = (
+        option_env!("CARGO_PKG_VERSION_MAJOR"),
+        option_env!("CARGO_PKG_VERSION_MINOR"),
+        option_env!("CARGO_PKG_VERSION_PATCH"),
+    );
+    match (maj, min, pat) {
+        (Some(maj), Some(min), Some(pat)) =>
+            format!("{}.{}.{}", maj, min, pat),
+        _ => "".to_string(),
+    }
+}
+
 struct SSRC(u32);
 struct RtpTimestamp(u32);
 struct NtpTimestamp(u64);
