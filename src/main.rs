9e556b5f1537110ce966a0a9d92d8172f19afb1d
Colin Perkins <csp@csperkins.org>
2015-01-10 22:23:12 +0000

    Fix Cargo version handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 // ================================================================================================
 
-pub fn version() -> String {
-    let (maj, min, pat) = (
-        option_env!("CARGO_PKG_VERSION_MAJOR"),
-        option_env!("CARGO_PKG_VERSION_MINOR"),
-        option_env!("CARGO_PKG_VERSION_PATCH"),
-    );
-    match (maj, min, pat) {
-        (Some(maj), Some(min), Some(pat)) =>
-            format!("{}.{}.{}", maj, min, pat),
-        _ => "".to_string(),
-    }
-}
+const CRTP_VERSION : &'static str = env!("CARGO_PKG_VERSION");
 
 struct SSRC(u32);
 struct RtpTimestamp(u32);
