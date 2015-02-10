3af6ca24969be4d0b8fe1c4ebe1b7ce8c213561c
Colin Perkins <csp@csperkins.org>
2015-02-10 19:22:35 +0000

    Remove Encodable impl Fix type of ntp_ts

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,10 @@ struct RtpTimestamp(u32);
 struct NtpTimestamp(u64);
 
 struct SenderInfo {
-  ntp_ts     : u32, // FIXME: should be NtpTimestamp,
+  ntp_ts     : u64, // FIXME: should be NtpTimestamp,
   rtp_ts     : u32, // FIXME: should be RtpTimestamp,
   pckt_count : u32,
   byte_count : u32
-}
-
-impl Encodable for SenderInfo {
-  fn encode<S : Encoder>(&self, encoder : &mut S) -> Result<(), S::Error> {
-    encoder.emit_u32(self.ntp_ts)
-  }
 }
 
 struct ReportBlock {
